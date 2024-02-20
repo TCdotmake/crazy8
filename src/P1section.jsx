cbfa95428cfee3a4d6cafb5e587b48d9e4255f1e
Tommy Chen <tommyjchen@hotmail.com>
2024-02-19 21:57:22 -0500

    cards are displayed in rows not exceeding 9 per row

--- src/P1section.jsx
+++ src/P1section.jsx
@@ -18,9 +18,18 @@ const containerCss = css`
   justify-content: center;
   align-item: center;
 `;
+
 export function P1section() {
+  const ARRSIZE = 8;
   const game = useContext(GameContext);
   const pile = game.p1Pile;
+  const copy = structuredClone(pile);
+  const multiArr = [];
+  while (copy.length > ARRSIZE) {
+    const sub = copy.splice(0, ARRSIZE);
+    multiArr.push(sub);
+  }
+  multiArr.push(copy);
 
   const handlePlay = (e) => {
     if (game.playerTurn) {
@@ -29,8 +38,15 @@ export function P1section() {
     }
   };
   return (
-    <div css={containerCss}>
-      {pile.length > 0 &&
+    <div
+      css={[
+        containerCss,
+        css`
+          flex-direction: column;
+        `,
+      ]}
+    >
+      {/* {pile.length > 0 &&
         pile.map((n, index) => {
           let alt = n.value + " of " + n.suit;
           let mid = (pile.length - 1) / 2;
@@ -55,6 +71,41 @@ export function P1section() {
                 data-key={n.key}
               ></img>
             </button>
+          );
+        })} */}
+
+      {multiArr.length > 0 &&
+        multiArr.map((sub) => {
+          return (
+            <div css={containerCss}>
+              {sub.length > 0 &&
+                sub.map((n, index) => {
+                  let alt = n.value + " of " + n.suit;
+                  let mid = (sub.length - 1) / 2;
+                  let offset = index - mid;
+                  let margin = offset > 0 ? "marginLeft" : "marginRight";
+                  let val = Math.abs(offset) * 20 * -1;
+
+                  return (
+                    <button
+                      css={css`
+                        transform: translateX(calc(-85px * ${offset}));
+                      `}
+                      onClick={handlePlay}
+                      key={n.key + "btn"}
+                      data-key={n.key}
+                    >
+                      <img
+                        css={cardSizeCss}
+                        key={n.key + "p1"}
+                        src={n.image}
+                        alt={alt}
+                        data-key={n.key}
+                      ></img>
+                    </button>
+                  );
+                })}
+            </div>
           );
         })}
     </div>
